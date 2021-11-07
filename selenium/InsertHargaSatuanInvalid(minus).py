abcc5a9fb4d9cb077b7c52d43a6585aaf2cf0987
kadabengarann <rio27dh@gmail.com>
2021-11-08 04:19:15 +0800

    change url selenium

--- selenium/InsertHargaSatuanInvalid(minus).py
+++ selenium/InsertHargaSatuanInvalid(minus).py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 driver = webdriver.Edge(r"browser\msedgedriver.exe")
 
 # URL of website
-url = "http://127.0.0.1"
+url = "http://127.0.0.1/RPL-Unit-Testing/"
 
 # Opening the website
 driver.get(url)
